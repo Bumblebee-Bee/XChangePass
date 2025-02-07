aa0b0e48750874e131a2a8db8bc1f67174c4f8c0
Si-rauis <kkyd7189@gmail.com>
2025-02-07 12:03:02 +0900

    🎉 Start : 개발 환경 세팅

---
+++ src/main/java/bumblebee/xchangepass/XChangePassApplication.java
@@ -0,0 +1,13 @@
+package bumblebee.xchangepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XChangePassApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XChangePassApplication.class, args);
+  }
+
+}
---
+++ src/test/java/bumblebee/xchangepass/XChangePassApplicationTests.java
@@ -0,0 +1,13 @@
+package bumblebee.xchangepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XChangePassApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
