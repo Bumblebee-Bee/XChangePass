c534b783ebb40ee42b75af94e326b4445c4ddc4b
usingjun <usingjun99@gmail.com>
2025-02-14 11:16:10 +0900

    :sparkles: 전반적인 User 기능 생성

---
+++ src/main/java/bumblebee/xchangepass/domain/user/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package bumblebee.xchangepass.domain.user.service;
+
+import bumblebee.xchangepass.domain.user.dto.CustomUserDetails;
+import bumblebee.xchangepass.domain.user.entity.User;
+import bumblebee.xchangepass.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByUserEmail(email)
+                .orElseThrow(ErrorCode.MEMBER_NOT_FOUND::commonException);
+
+        if (user != null){
+
+            return new CustomUserDetails(user);
+        }
+        return null;
+    }
+}
